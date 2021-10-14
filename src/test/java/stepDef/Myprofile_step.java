a92bd0ae1d75725c4399edf0d3ccdb9603d763a3
Md Nazmul Hossain <nazmulny15@gmail.com>
2021-10-13 23:27:35 -0400

    homework3

---
+++ src/test/java/stepDef/Myprofile_step.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class Myprofile_step {
+    @Then("I should be able to successfully log in")
+    public void iShouldBeAbleToSuccessfullyLogIn() {
+    }
+}
