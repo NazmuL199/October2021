902f039a927a7fb7180542f87b7a81bd59b465fb
Md Nazmul Hossain <nazmulny15@gmail.com>
2021-11-10 11:55:40 -0500

    bakhtiyer

---
+++ src/test/java/stepDef/actual_page.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class actual_page {
+    @Then("I should see Thank you for sign up message")
+    public void iShouldSeeThankYouForSignUpMessage() {
+    }
+}
---
+++ src/test/java/stepDef/signup_page.java
@@ -0,0 +1,21 @@
+package stepDef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+public class signup_page {
+
+    @And("I click Create New Account Button in Login Page")
+    public void iClickCreateNewAccountButtonInLoginPage() {
+        
+    }
+
+    @And("I enter valid student information")
+    public void iEnterValidStudentInformation() {
+        
+    }
+
+    @When("I click on Create my account button")
+    public void iClickOnCreateMyAccountButton() {
+    }
+}
